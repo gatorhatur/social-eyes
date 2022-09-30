dd1850a2500ba673e7c18e0e143f737c0ca7d5d8
Brandon Davis <brancdavis@gmail.com>
2022-09-30 12:27:22 -0400

    wireframed thought controller

---
+++ controllers/thought-controller.js
@@ -0,0 +1,27 @@
+const models = require('../models');
+
+module.exports = {
+    // /api/thoughts
+    getAllThoughts(req, res) {
+        
+    },
+    getThoughtById({ params }, res) {
+        
+    },
+    createThought({ body }, res) {
+        
+    },
+    updateThought({ params, body }, res) {
+        
+    },
+    deleteThought({ params }, res) {
+        
+    },
+    // /api/thoughts/:thoughtId/reactions
+    addReaction({ params, body }, res) {
+        
+    },
+    deleteReaction({ params }, res) {
+        
+    }
+}
